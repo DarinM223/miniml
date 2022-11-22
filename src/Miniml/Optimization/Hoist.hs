f17215c74fe954fa7ea121bc83e935e10d66f61f
D M <darinm223@gmail.com>
2022-11-21 23:07:55 -0800

    Hoisting attempts to push down Fixes, Selects, and Offsets first
    Pushing down purely functional primops like `alength` and `slength` not
    implemented yet.

--- src/Miniml/Optimization/Hoist.hs
+++ src/Miniml/Optimization/Hoist.hs
@@ -1,9 +1,29 @@
 module Miniml.Optimization.Hoist where
 
 import Data.Foldable (foldl')
+import Data.Functor.Foldable (cata)
 import Data.IntSet qualified as IS
+import Data.Maybe (fromMaybe)
 import Data.Tuple.Extra (fst3)
-import Miniml.Cps (Cexp (..), Value (..), Var)
+import Miniml.Cps (Cexp (..), CexpF (..), Value (..), Var)
+
+constr :: Cexp -> Cexp -> Cexp
+constr (Record vl w _) = Record vl w
+constr (Select i v w _) = Select i v w
+constr (Offset i v w _) = Offset i v w
+constr (App f vl) = const (App f vl)
+constr (Fix fl _) = Fix fl
+constr (Switch v _) = \e -> Switch v [e]
+constr (Primop op vl wl _) = \e -> Primop op vl wl [e]
+
+bindings :: Cexp -> [Var]
+bindings (Record _ w _) = [w]
+bindings (Select _ _ w _) = [w]
+bindings (Offset _ _ w _) = [w]
+bindings (App _ _) = []
+bindings (Fix fl _) = fst3 <$> fl
+bindings (Switch _ _) = []
+bindings (Primop _ _ wl _) = wl
 
 fixFreeVars :: [Var] -> (Var, [Var], Cexp) -> IS.IntSet
 fixFreeVars fl (_, vl, e) = freeVars e IS.\\ IS.fromList (fl ++ vl)
@@ -37,93 +57,90 @@ freeVars = go IS.empty
 -- Only returns the free variables of the fixes because
 -- it is only used to check for scope rules when hoisting upward.
 hoist :: Cexp -> (IS.IntSet, Cexp)
-hoist = go
+hoist = cata go
   where
-    go (Record vl w e) = (m, Record vl w e')
+    go :: CexpF (IS.IntSet, Cexp) -> (IS.IntSet, Cexp)
+    go (RecordF vl w (_, e)) = (IS.empty, Record vl w e)
+    go (SelectF i v w (m, e)) =
+      case e of
+        Fix fl e' | not (IS.member w m) -> (m, Fix fl (push e'))
+        _ -> (IS.empty, push e)
       where
-        (m, e') = go e
-    go (Select i v w e) =
-      case e' of
-        Fix fl e'' | not (IS.member w m) -> (m, Fix fl (Select i v w e''))
-        _ -> (m, Select i v w e')
+        push e0 = fromMaybe (Select i v w e0) (pushDown (Select i v w e0) e0)
+    go (OffsetF i v w (m, e)) =
+      case e of
+        Fix fl e' | not (IS.member w m) -> (m, Fix fl (push e'))
+        _ -> (IS.empty, push e)
       where
-        (m, e') = go e
-    go (Offset i v w e) =
-      case e' of
-        Fix fl e'' | not (IS.member w m) -> (m, Fix fl (Select i v w e''))
-        _ -> (m, Select i v w e')
-      where
-        (m, e') = go e
-    go (App f vl) = (IS.empty, App f vl)
+        push e0 = fromMaybe (Offset i v w e0) (pushDown (Offset i v w e0) e0)
+    go (AppF f vl) = (IS.empty, App f vl)
     -- TODO(DarinM223): first split the known from the closure requiring functions.
-    go (Fix fl e) =
-      case e' of
-        Fix fl'' e'' -> (IS.union free m, Fix (fl' ++ fl'') e'')
-        _ -> (free, Fix fl' e')
+    go (FixF fl (m, e)) =
+      case e of
+        Fix fl'' e' ->
+          case pushDown (Fix fl' e) e' of
+            Just e'' -> (m, Fix fl'' e'')
+            Nothing -> (IS.union free' m, Fix (fl' ++ fl'') e')
+        _ ->
+          case pushDown (Fix fl' e) e of
+            Just e' -> (IS.empty, e')
+            Nothing -> (free', Fix fl' e)
       where
-        free = IS.unions (fixFreeVars (fmap fst3 fl') <$> fl')
-        fl' = foldr goFn [] fl
-        (m, e') = go e
-    go (Switch v cl)
+        free' = free IS.\\ IS.fromList (fmap fst3 fl')
+        (free, fl') = foldr goFn (IS.empty, []) fl
+    go (SwitchF v cl)
       | null fl = (m, Switch v cl')
       | otherwise = (m, Fix fl (Switch v cl'))
       where
         (m, fl, cl') = foldr (goBranch []) (IS.empty, [], []) cl
-    go (Primop op vl wl cl)
+    go (PrimopF op vl wl cl)
       | null fl = (m, Primop op vl wl cl')
       | otherwise = (m, Fix fl (Primop op vl wl cl'))
       where
         (m, fl, cl') = foldr (goBranch wl) (IS.empty, [], []) cl
 
-    goBranch wl e (free, fl, result) = case e' of
-      Fix fl' e''
+    goBranch wl (m, e) (free, fl, result) = case e of
+      Fix fl' e'
         | not (any (`IS.member` m) wl) ->
-            (IS.union free m, fl' ++ fl, e'' : result)
-      _ -> (free, fl, e' : result)
+            (IS.union free m, fl' ++ fl, e' : result)
+      _ -> (free, fl, e : result)
+    goFn (f, vl, (m, e)) (free, result) =
+      case e of
+        Fix fl' e'
+          | not (any (`IS.member` m) vl) ->
+              (free' e', (f, vl, e') : fl' ++ result)
+        _ -> (free' e, (f, vl, e) : result)
       where
-        (m, e') = go e
-    goFn (f, vl, e) result =
-      case e' of
-        Fix fl' e''
-          | not (any (`IS.member` m) vl) -> (f, vl, e'') : fl' ++ result
-        _ -> (f, vl, e') : result
+        free' body = IS.unions [free, m, freeVars body] IS.\\ IS.fromList vl
+
+pushDown :: Cexp -> Cexp -> Maybe Cexp
+pushDown = go
+  where
+    go _ (Record {}) = Nothing
+    go e (Select i v w rest)
+      | v `elem` (Var <$> bindings e) = Nothing
+      | otherwise = Select i v w <$> go e rest
+    go e (Offset i v w rest)
+      | v `elem` (Var <$> bindings e) = Nothing
+      | otherwise = Offset i v w <$> go e rest
+    go _ (App {}) = Nothing
+    go (Fix fl' _) (Fix fl rest) = Just $ fromMaybe fix $ pushDown fix rest
       where
-        (m, e') = go e
+        fix = Fix (fl ++ fl') rest
+    go e (Fix fl rest) = Fix fl <$> go e rest
+    go e (Switch v el) = pushBranch e (Switch v) el
+    go e (Primop op vl wl [rest])
+      | any (`elem` (Var <$> bindings e)) vl = Nothing
+      | otherwise = Primop op vl wl . (: []) <$> go e rest
+    go e (Primop op vl wl el) = pushBranch e (Primop op vl wl) el
 
--- pushDown :: Cexp -> Maybe Cexp
--- pushDown = undefined
---   where
---     go (Record vl w e) = (m', Record vl w e')
---       where
---         m' = IS.delete w $ IS.union (freeList (fmap fst vl)) m
---         (m, e') = go e
---     go (Select i v w e) = (m', Select i v w e')
---       where
---         m' = IS.delete w $ maybe m (`IS.insert` m) (var v)
---         (m, e') = go e
---     go (Offset i v w e) = (m', Offset i v w e')
---       where
---         m' = IS.delete w $ maybe m (`IS.insert` m) (var v)
---         (m, e') = go e
---     go (App f vl) = (freeList (f : vl), App f vl)
---     go (Fix fl e) = (m', Fix fl' e')
---       where
---         m' = IS.union free $ m IS.\\ IS.fromList (fmap fst3 fl)
---         (free, fl') = foldr (goFn (fmap fst3 fl)) (IS.empty, []) fl
---         (m, e') = go e
---     go (Switch v cl) = (m', Switch v cl')
---       where
---         m' = maybe m (`IS.insert` m) (var v)
---         (m, cl') = foldr goBranch (IS.empty, []) cl
---     go (Primop op vl wl cl) = (m', Primop op vl wl cl')
---       where
---         m' = IS.union (freeList vl) m IS.\\ IS.fromList wl
---         (m, cl') = foldr goBranch (IS.empty, []) cl
-
---     goBranch e (free, result) = (IS.union free m, e' : result)
---       where
---         (m, e') = go e
---     goFn fl (f, vl, e) (free, result) = (IS.union free m', (f, vl, e') : result)
---       where
---         m' = m IS.\\ IS.fromList (fl ++ vl)
---         (m, e') = go e
+    pushBranch e0 mkE el
+      | length (filter canPushDown ms) == 1 = Just (mkE el')
+      | otherwise = Nothing
+      where
+        el' = uncurry branch <$> zip ms el
+        branch m e = if canPushDown m then fromMaybe e' (pushDown e' e) else e
+          where
+            e' = constr e0 e
+        ms = fmap freeVars el
+        canPushDown m = any (`IS.member` m) (bindings e0)
--- test/Optimization/Hoist.hs
+++ test/Optimization/Hoist.hs
@@ -1,7 +1,7 @@
 module Optimization.Hoist (tests) where
 
 import Miniml.Cps qualified as Cps
-import Miniml.Optimization.Hoist (hoist)
+import Miniml.Optimization.Hoist (hoist, pushDown)
 import Miniml.Shared (Primop (..))
 import Test.Tasty (TestTree, testGroup)
 import Test.Tasty.HUnit (testCase, (@?=))
@@ -10,8 +10,177 @@ tests :: TestTree
 tests =
   testGroup
     "Hoisting"
-    [ testCase "Simple hoisting up fix" testSimpleFixHoist
+    [ testCase "Pushes fix down into branches" testFixPushSwitch,
+      testCase
+        "Doesn't push fix down switch when used in two branches"
+        testFixNoPushSwitch,
+      testCase
+        "If succeeds, then fails trying to continue, return the last success"
+        testLastSucceed,
+      testCase "Simple hoisting up fix" testSimpleFixHoist,
+      testCase
+        "Hoisting pushes selects and offsets into switches"
+        testHoistPushSwitch
     ]
+
+testFixPushSwitch :: IO ()
+testFixPushSwitch = do
+  let e =
+        Cps.Select
+          1
+          (Cps.Var 6)
+          7
+          ( Cps.Offset
+              2
+              (Cps.Var 6)
+              8
+              ( Cps.Switch
+                  (Cps.Var 8)
+                  [ Cps.Primop
+                      Plus
+                      [Cps.Int 1, Cps.Int 2]
+                      [9]
+                      [Cps.App (Cps.Var 0) [Cps.Var 9]],
+                    Cps.Fix
+                      [(10, [11], Cps.App (Cps.Var 11) [Cps.Int 3])]
+                      ( Cps.Primop
+                          Gt
+                          [Cps.Int 1, Cps.Int 2]
+                          []
+                          [ Cps.App (Cps.Var 0) [Cps.Int 1],
+                            Cps.Fix
+                              [(12, [13], Cps.App (Cps.Var 13) [Cps.Int 4])]
+                              (Cps.App (Cps.Var 4) [Cps.Int 1])
+                          ]
+                      )
+                  ]
+              )
+          )
+      fl =
+        [ (1, [2, 3], Cps.App (Cps.Var 3) [Cps.Int 1]),
+          (4, [5], Cps.App (Cps.Var 5) [Cps.Int 2])
+        ]
+  pushDown (Cps.Fix fl e) e
+    @?= Just
+      ( Cps.Select
+          1
+          (Cps.Var 6)
+          7
+          ( Cps.Offset
+              2
+              (Cps.Var 6)
+              8
+              ( Cps.Switch
+                  (Cps.Var 8)
+                  [ Cps.Primop
+                      Plus
+                      [Cps.Int 1, Cps.Int 2]
+                      [9]
+                      [Cps.App (Cps.Var 0) [Cps.Var 9]],
+                    Cps.Primop
+                      Gt
+                      [Cps.Int 1, Cps.Int 2]
+                      []
+                      [ Cps.App (Cps.Var 0) [Cps.Int 1],
+                        Cps.Fix
+                          [ (12, [13], Cps.App (Cps.Var 13) [Cps.Int 4]),
+                            (10, [11], Cps.App (Cps.Var 11) [Cps.Int 3]),
+                            (1, [2, 3], Cps.App (Cps.Var 3) [Cps.Int 1]),
+                            (4, [5], Cps.App (Cps.Var 5) [Cps.Int 2])
+                          ]
+                          (Cps.App (Cps.Var 4) [Cps.Int 1])
+                      ]
+                  ]
+              )
+          )
+      )
+
+testFixNoPushSwitch :: IO ()
+testFixNoPushSwitch = do
+  let e =
+        Cps.Select
+          1
+          (Cps.Var 6)
+          7
+          ( Cps.Offset
+              2
+              (Cps.Var 6)
+              8
+              ( Cps.Switch
+                  (Cps.Var 8)
+                  [ Cps.Primop
+                      Plus
+                      [Cps.Int 1, Cps.Int 2]
+                      [9]
+                      [Cps.App (Cps.Var 1) [Cps.Int 2, Cps.Var 9]],
+                    Cps.App (Cps.Var 4) [Cps.Int 1]
+                  ]
+              )
+          )
+      fl = [(1, [2, 3], Cps.App (Cps.Var 3) [Cps.Int 1]), (4, [5], Cps.App (Cps.Var 5) [Cps.Int 2])]
+  pushDown (Cps.Fix fl e) e @?= Nothing
+
+testLastSucceed :: IO ()
+testLastSucceed = do
+  let e =
+        Cps.Select
+          1
+          (Cps.Var 6)
+          7
+          ( Cps.Offset
+              2
+              (Cps.Var 6)
+              8
+              ( Cps.Switch
+                  (Cps.Var 8)
+                  [ Cps.App (Cps.Var 0) [Cps.Int 1],
+                    Cps.Switch
+                      (Cps.Var 8)
+                      [ Cps.Primop
+                          Plus
+                          [Cps.Int 1, Cps.Int 2]
+                          [9]
+                          [Cps.App (Cps.Var 1) [Cps.Int 2, Cps.Var 9]],
+                        Cps.App (Cps.Var 4) [Cps.Int 1]
+                      ]
+                  ]
+              )
+          )
+      fl = [(1, [2, 3], Cps.App (Cps.Var 3) [Cps.Int 1]), (4, [5], Cps.App (Cps.Var 5) [Cps.Int 2])]
+  pushDown (Cps.Fix fl e) e
+    @?= Just
+      ( Cps.Select
+          1
+          (Cps.Var 6)
+          7
+          ( Cps.Offset
+              2
+              (Cps.Var 6)
+              8
+              ( Cps.Switch
+                  (Cps.Var 8)
+                  [ Cps.App (Cps.Var 0) [Cps.Int 1],
+                    Cps.Fix
+                      [ (1, [2, 3], Cps.App (Cps.Var 3) [Cps.Int 1]),
+                        (4, [5], Cps.App (Cps.Var 5) [Cps.Int 2])
+                      ]
+                      ( Cps.Switch
+                          (Cps.Var 8)
+                          [ Cps.Primop
+                              Plus
+                              [Cps.Int 1, Cps.Int 2]
+                              [9]
+                              [ Cps.App
+                                  (Cps.Var 1)
+                                  [Cps.Int 2, Cps.Var 9]
+                              ],
+                            Cps.App (Cps.Var 4) [Cps.Int 1]
+                          ]
+                      )
+                  ]
+              )
+          )
+      )
 
 testSimpleFixHoist :: IO ()
 testSimpleFixHoist = do
@@ -68,14 +237,54 @@ testSimpleFixHoist = do
                     9
                     ( Cps.Fix
                         [ (10, [11], Cps.App (Cps.Var 0) [Cps.Var 9]),
-                          (12, [13], Cps.App (Cps.Var 0) [Cps.Var 3])
+                          (12, [13], Cps.App (Cps.Var 0) [Cps.Var 3]),
+                          (7, [8], Cps.App (Cps.Var 0) [Cps.Var 8])
                         ]
                         (Cps.App (Cps.Var 12) [Cps.Int 1])
                     )
                 ),
-                (7, [8], Cps.App (Cps.Var 0) [Cps.Var 8]),
                 (14, [15], Cps.App (Cps.Var 0) [Cps.Var 4])
               ]
               (Cps.App (Cps.Var 14) [Cps.Int 1])
           ]
       ]
+
+testHoistPushSwitch :: IO ()
+testHoistPushSwitch = do
+  let e =
+        Cps.Select
+          1
+          (Cps.Var 1)
+          2
+          ( Cps.Offset
+              2
+              (Cps.Var 2)
+              3
+              ( Cps.Switch
+                  (Cps.Var 1)
+                  [ Cps.Fix
+                      [(4, [5], Cps.App (Cps.Var 5) [Cps.Int 1])]
+                      (Cps.App (Cps.Var 0) [Cps.Int 2]),
+                    Cps.App (Cps.Var 0) [Cps.Var 3]
+                  ]
+              )
+          )
+      (_, e') = hoist e
+  e'
+    @?= Cps.Fix
+      [(4, [5], Cps.App (Cps.Var 5) [Cps.Int 1])]
+      ( Cps.Switch
+          (Cps.Var 1)
+          [ Cps.App (Cps.Var 0) [Cps.Int 2],
+            Cps.Select
+              1
+              (Cps.Var 1)
+              2
+              ( Cps.Offset
+                  2
+                  (Cps.Var 2)
+                  3
+                  (Cps.App (Cps.Var 0) [Cps.Var 3])
+              )
+          ]
+      )
